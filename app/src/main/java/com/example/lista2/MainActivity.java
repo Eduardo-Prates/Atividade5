6507244abd128786c3389495c7e4cfa0759700d6
Eduardo-Prates <eduardotiadoro@gmail.com>
2022-02-21 23:20:34 -0300

    ScrollView

--- app/src/main/java/com/example/lista2/MainActivity.java
+++ app/src/main/java/com/example/lista2/MainActivity.java
@@ -18,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
 
         EditText e1 = (EditText) findViewById(R.id.nome);
         EditText e2 = (EditText) findViewById(R.id.senha);
-        EditText e3 = (EditText) findViewById(R.id.dataVenc);
+        EditText e3 = (EditText) findViewById(R.id.telefone);
+        EditText e4 = (EditText) findViewById(R.id.senhaOrkut);
+        EditText e5 = (EditText) findViewById(R.id.email);
 
         TextView t1 = (TextView) findViewById(R.id.printNome);
 
@@ -27,8 +29,8 @@ public class MainActivity extends AppCompatActivity {
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String t1Texto = "Nome: "+e1.getText()+"Senha :"+e2.getText()+"Vencimento: "+e3.getText();
-                t1.setText("Nome: "+e1.getText()+"\nSenha :"+e2.getText()+"\nVencimento: "+e3.getText() );
+                String t1Texto = e1.getText()+"-"+e2.getText()+"-"+e3.getText()+"-"+e4.getText()+"-"+e5.getText();
+                t1.setText("Nome: "+e1.getText()+"\nSenha :"+e2.getText()+"\nTelefone: "+e3.getText()+"\nSenha do Orkut: "+e4.getText()+"\nEmail: "+e5.getText() );
                 Toast.makeText(getApplicationContext(), t1Texto.toString(), Toast.LENGTH_SHORT).show();
             }
         });
